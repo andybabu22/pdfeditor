2017ae5ca02927c38c7d652b5d28fa4e780b7a04
andybabu22 <andybabau8@gmail.com>
2025-10-17 04:46:13 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: { extend: {} },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
